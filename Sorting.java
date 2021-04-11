404cc7899fb6cfc9ffbf3d4c4b0e01481819c8a8
ShubhamPorwal31 <82319294+ShubhamPorwal31@users.noreply.github.com>
2021-04-11 06:19:38 -0700

    Normal Sorting on basis of for loops

---
+++ Sorting.java
@@ -0,0 +1,26 @@
+package primeNumer;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int a[] = {77,14,99,23,45};
+		int temp;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]>a[j])
+				{
+				   temp = a[i];
+				   a[i] = a[j];
+				   a[j]= temp;
+				}
+			}
+			System.out.println(a[i]);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
